0772056e394e5df919f0137dd45573046a5ef8db
ghooman <mailtoghoo@gmail.com>
2024-08-22 19:01:06 +0900

    add: calculate

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,19 +6,45 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setxIsNext] = useState(true);
 
-  const status = `Next player ${xIsNext ? `X` : `0`}`;
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setxIsNext((prev) => !prev);
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        console.log(a);
+        return squares[a];
+      }
+    }
+
+    const winner = calculateWinner(squares);
+
+    const status = `Next player ${xIsNext ? `X` : `0`}`;
+
+    const handleClick = (i) => {
+      const newSquares = squares.slice();
+      newSquares[i] = xIsNext ? "X" : "O";
+      setSquares(newSquares);
+      setxIsNext((prev) => !prev);
+    };
+
+    const renderSquare = (i) => {
+      return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    };
   };
-
-  const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  };
-
   return (
     <div>
       <div className="status">{status}</div>
